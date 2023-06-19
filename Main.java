c1d66d8a15519503d06680da4fe15134340ec70d
ArthurSanturio <89260748+ArthurSanturio@users.noreply.github.com>
2023-06-19 16:37:19 -0300

    att

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 //import ui.FormularioCliente;
-import ui.FormularioTipoCarga;
+//import ui.FormularioTipoCarga;
 //import ui.FormularioNavio;
 //import ui.FormularioPorto;
+import ui.FormularioPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-        new FormularioTipoCarga();
+        new FormularioPrincipal();
+        //new FormularioTipoCarga();
         //new FormularioCliente();
         //new FormularioNavio();
         //new FormularioPorto();
